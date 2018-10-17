43dea2bea9cbfcb258dcd3da9604bc1765f12c33
James Fletcher <james@grakn.ai>
2018-10-17 15:06:05 +0100

    Find the supertypes of a concept ready to do encoding

--- grakn_graphsage/src/schema/traversal.py
+++ grakn_graphsage/src/schema/traversal.py
@@ -1,6 +1,5 @@
 import grakn
 
-
 GET_THING_TYPES_QUERY = "match $x sub thing; get;"
 GET_ROLE_TYPES_QUERY = "match $x sub role; get;"
 
@@ -13,17 +12,32 @@ def labels_from_types(schema_concept_types):
         yield type.label()
 
 
-def get_schema_concept_types(grakn_tx, get_types_query, include_implicit=True, include_metatypes=False):
+def get_sups_labels_per_type(schema_concept_types, include_metatypes=False, include_self=False):
+    for schema_concept_type in schema_concept_types:
+        super_types = schema_concept_type.sups()
 
-    schema_concepts = []
+        supertype_labels = []
+        for super_type in super_types:
+            super_type_label = super_type.label()
 
-    for answer in grakn_tx.query(get_types_query):
-        t = answer.get('x')
+            if not (((not include_self) and super_type_label == schema_concept_type.label()) or (
+                (not include_metatypes) and super_type.label() in METATYPE_LABELS)):
+                supertype_labels.append(super_type.label())
+        yield schema_concept_type.label(), supertype_labels
 
-        if not (((not include_implicit) and t.is_implicit()) or (
-                (not include_metatypes) and t.label() in METATYPE_LABELS)):
-            schema_concepts.append(t.label())
-            yield t
+
+class TraversalExecutor:
+    def __init__(self, grakn_tx):
+        self._grakn_tx = grakn_tx
+
+    def get_schema_concept_types(self, get_types_query, include_implicit=True, include_metatypes=False):
+
+        for answer in self._grakn_tx.query(get_types_query):
+            t = answer.get('x')
+
+            if not (((not include_implicit) and t.is_implicit()) or (
+                    (not include_metatypes) and t.label() in METATYPE_LABELS)):
+                yield t
 
 
 if __name__ == '__main__':
@@ -31,11 +45,25 @@ if __name__ == '__main__':
     session = client.session(keyspace="test_schema")
     tx = session.transaction(grakn.TxType.WRITE)
 
-    schema_concept_types = get_schema_concept_types(tx, GET_THING_TYPES_QUERY, include_implicit=True, include_metatypes=False)
+    print("================= THINGS ======================")
+    te = TraversalExecutor(tx)
+    schema_concept_types = te.get_schema_concept_types(GET_THING_TYPES_QUERY, include_implicit=True, include_metatypes=False)
     labels = labels_from_types(schema_concept_types)
     print(list(labels))
 
-    print("=======================================")
-    schema_concept_types = get_schema_concept_types(tx, GET_ROLE_TYPES_QUERY, include_implicit=True, include_metatypes=False)
+    schema_concept_types = te.get_schema_concept_types(GET_THING_TYPES_QUERY, include_implicit=True, include_metatypes=False)
+    super_types = get_sups_labels_per_type(schema_concept_types, include_self=True, include_metatypes=False)
+    print("==== super types ====")
+    [print(super_type) for super_type in super_types]
+
+    print("================= ROLES ======================")
+    schema_concept_types = te.get_schema_concept_types(GET_ROLE_TYPES_QUERY, include_implicit=True, include_metatypes=False)
     labels = labels_from_types(schema_concept_types)
     print(list(labels))
+
+    schema_concept_types = te.get_schema_concept_types(GET_ROLE_TYPES_QUERY, include_implicit=True, include_metatypes=False)
+    super_types = get_sups_labels_per_type(schema_concept_types, include_self=True, include_metatypes=False)
+    print("==== super types ====")
+    [print(super_type) for super_type in super_types]
+
+
