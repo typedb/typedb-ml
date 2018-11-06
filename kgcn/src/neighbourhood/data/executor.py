7108b52c3970b9cff85b2e2afd49caee7649821b
James Fletcher <james@grakn.ai>
2018-11-06 15:08:41 +0000

    Improves test hierarchy, adds useful tests for data executor, minor changes to executor

--- kgcn/src/neighbourhood/data/executor.py
+++ kgcn/src/neighbourhood/data/executor.py
@@ -15,14 +15,14 @@ class TraversalExecutor:
     # TODO Changing queries due to bug
     # query = "match $x id {}; $relationship($role: $x); get $relationship, $role;")
     ROLES_PLAYED_QUERY = {
-        'query': "match $x id {}; $relationship($x); get $relationship;",
+        'query': "match $x id {}; $relationship($role: $x); get $relationship, $role;",
         'role_variable': 'role',
         'role_direction': TARGET_PLAYS,
         'neighbour_variable': 'relationship'}
 
     # query = "match $relationship id {}; $relationship($role: $x) isa {}; get $x, $role;"
     ROLEPLAYERS_QUERY = {
-        'query': "match $relationship id {}; $relationship($x) isa {}; get $x;",
+        'query': "match $relationship id {}; $relationship($role: $x); get $x, $role;",
         'role_variable': 'role',
         'role_direction': NEIGHBOUR_PLAYS,
         'neighbour_variable': 'x'}
@@ -32,37 +32,37 @@ class TraversalExecutor:
         self.roles_played_query = roles_played_query
         self.roleplayers_query = roleplayers_query
 
-    def __call__(self, query_direction, *args):
-            """
-            Takes a query to execute and the variables to return
-            :param query_direction: whether we want to retrieve roles played or role players
-            :param concept_id: id for the concept to find connections for
-            :return:
-            """
+    def __call__(self, query_direction, concept_id):
+        """
+        Takes a query to execute and the variables to return
+        :param query_direction: whether we want to retrieve roles played or role players
+        :param concept_id: id for the concept to find connections for
+        :return:
+        """
 
-            if query_direction == ROLES_PLAYED:
-                base_query = self.ROLES_PLAYED_QUERY
-            elif query_direction == ROLEPLAYERS:
-                base_query = self.ROLEPLAYERS_QUERY
-            else:
-                raise ValueError('query_direction isn\'t properly defined')
+        if query_direction == ROLES_PLAYED:
+            base_query = self.ROLES_PLAYED_QUERY
+        elif query_direction == ROLEPLAYERS:
+            base_query = self.ROLEPLAYERS_QUERY
+        else:
+            raise ValueError('query_direction isn\'t properly defined')
 
-            query = base_query['query'].format(*args)
-            print(query)
-            roles_iterator = self._grakn_tx.query(query)
+        query = base_query['query'].format(concept_id)
+        print(query)
+        roles_iterator = self._grakn_tx.query(query)
 
-            def _roles_iterator():
-                for answer in roles_iterator:
-                    # TODO See above, omitting due to bug
-                    # role_label = answer.get(base_query['role_variable']).label()
-                    role_label = UNKNOWN_ROLE_TARGET_PLAYS_LABEL
-                    relationship_concept = answer.get(base_query['neighbour_variable'])
-                    relationship_info = build_concept_info(relationship_concept)
+        def _roles_iterator():
+            for answer in roles_iterator:
+                # TODO See above, omitting due to bug
+                role_label = answer.get(base_query['role_variable']).label()
+                # role_label = UNKNOWN_ROLE_TARGET_PLAYS_LABEL
+                relationship_concept = answer.get(base_query['neighbour_variable'])
+                relationship_info = build_concept_info(relationship_concept)
 
-                    yield {'role_label': role_label, 'role_direction': base_query['role_direction'],
-                           'neighbour_info': relationship_info}
+                yield {'role_label': role_label, 'role_direction': base_query['role_direction'],
+                       'neighbour_info': relationship_info}
 
-            return _roles_iterator()
+        return _roles_iterator()
 
 
 class ConceptInfo(utils.PropertyComparable):
--- kgcn/src/neighbourhood/data/executor_test.py
+++ kgcn/src/neighbourhood/data/executor_test.py
@@ -1,4 +1,3 @@
-
 import unittest
 
 import grakn
@@ -6,57 +5,76 @@ import grakn
 import kgcn.src.neighbourhood.data.executor as ex
 
 
-class GraknIntegrationTest(unittest.TestCase):
+class BaseGraknIntegrationTests:
+    class GraknIntegrationTest(unittest.TestCase):
 
-    session = None
+        session = None
 
-    @classmethod
-    def setUpClass(cls):
-        client = grakn.Grakn(uri="localhost:48555")
-        cls.session = client.session(keyspace="test_schema")
+        @classmethod
+        def setUpClass(cls):
+            client = grakn.Grakn(uri="localhost:48555")
+            cls.session = client.session(keyspace="test_schema")
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.session.close()
+        @classmethod
+        def tearDownClass(cls):
+            cls.session.close()
+
+        def setUp(self):
+            self._tx = self.session.transaction(grakn.TxType.WRITE)
+            self._executor = ex.TraversalExecutor(self._tx)
+            self._concept = list(self._tx.query(self.query))[0].get(self.var)
+
+        def test_role_is_in_neighbour_roles(self):
+            self.assertIn(self.role, [r['role_label'] for r in self._res])
+
+        def test_num_results(self):
+            self.assertEqual(len(self._res), self.num_results)
+
+
+class TestTraversalExecutorFromEntity(BaseGraknIntegrationTests.GraknIntegrationTest):
+
+    query = "match $x isa company, has name 'Google'; get;"
+    var = 'x'
+    role = 'employer'
+    num_results = 3
 
     def setUp(self):
-        self._tx = self.session.transaction(grakn.TxType.WRITE)
+        super(TestTraversalExecutorFromEntity, self).setUp()
+        self._res = list(self._executor(ex.TARGET_PLAYS, self._concept.id))
 
 
-class TestTraversalExecutor(GraknIntegrationTest):
+class TestTraversalExecutorFromRelationship(BaseGraknIntegrationTests.GraknIntegrationTest):
+
+    query = "match $x isa employment; get;"
+    var = 'x'
+    role = 'employer'
+    num_results = 2
 
     def setUp(self):
-        # self._tx = self.session.transaction(grakn.TxType.WRITE)
-        super(TestTraversalExecutor, self).setUp()
-        self._executor = ex.TraversalExecutor(self._tx)
-
-    def test_entity(self):
-
-        entity_query = "match $x isa company, has name 'Google'; get;"
-
-        concept = list(self._tx.query(entity_query))[0].get('x')
-        res = self._executor(ex.TARGET_PLAYS, concept.id)
+        super(TestTraversalExecutorFromRelationship, self).setUp()
+        self._res = list(self._executor(ex.NEIGHBOUR_PLAYS, self._concept.id))
 
 
-class TestBuildConceptInfo(GraknIntegrationTest):
-    def setUp(self):
-        super(TestBuildConceptInfo, self).setUp()
+class BaseTestBuildConceptInfo:
+    class TestBuildConceptInfo(BaseGraknIntegrationTests.GraknIntegrationTest):
+        def setUp(self):
+            super(BaseTestBuildConceptInfo.TestBuildConceptInfo, self).setUp()
 
-        self._concept = list(self._tx.query(self.query))[0].get(self.var)
+            self._concept = list(self._tx.query(self.query))[0].get(self.var)
 
-        self._concept_info = ex.build_concept_info(self._concept)
+            self._concept_info = ex.build_concept_info(self._concept)
 
-    def test_id(self):
-        self.assertEqual(self._concept_info.id, self._concept.id)
+        def test_id(self):
+            self.assertEqual(self._concept_info.id, self._concept.id)
 
-    def test_type_label(self):
-        self.assertEqual(self._concept_info.type_label, self.type_label)
+        def test_type_label(self):
+            self.assertEqual(self._concept_info.type_label, self.type_label)
 
-    def test_base_type_label(self):
-        self.assertEqual(self._concept_info.base_type_label, self.base_type)
+        def test_base_type_label(self):
+            self.assertEqual(self._concept_info.base_type_label, self.base_type)
 
 
-class TestBuildConceptInfoForEntity(TestBuildConceptInfo):
+class TestBuildConceptInfoForEntity(BaseTestBuildConceptInfo.TestBuildConceptInfo):
 
     query = "match $x isa company, has name 'Google'; get;"
     var = 'x'
@@ -64,7 +82,7 @@ class TestBuildConceptInfoForEntity(TestBuildConceptInfo):
     base_type = 'entity'
 
 
-class TestBuildConceptInfoForRelationship(TestBuildConceptInfo):
+class TestBuildConceptInfoForRelationship(BaseTestBuildConceptInfo.TestBuildConceptInfo):
 
     query = "match $x isa employment; get;"
     var = 'x'
@@ -72,16 +90,25 @@ class TestBuildConceptInfoForRelationship(TestBuildConceptInfo):
     base_type = 'relationship'
 
 
-class TestBuildConceptInfoForAttribute(TestBuildConceptInfo):
+class TestBuildConceptInfoForImplicitRelationship(BaseTestBuildConceptInfo.TestBuildConceptInfo):
 
-    def test_data_type(self):
-        self.assertEqual(self._concept_info.data_type, self.data_type)
+    query = "match $x isa @has-job-title; get;"
+    var = 'x'
+    type_label = '@has-job-title'
+    base_type = 'relationship'  # TODO do we want to see @has-attribute here?
 
-    def test_value(self):
-        self.assertEqual(self._concept_info.value, self.value)
+
+class BaseTestBuildConceptInfoForAttribute:
+    class TestBuildConceptInfoForAttribute(BaseTestBuildConceptInfo.TestBuildConceptInfo):
+
+        def test_data_type(self):
+            self.assertEqual(self._concept_info.data_type, self.data_type)
+
+        def test_value(self):
+            self.assertEqual(self._concept_info.value, self.value)
 
 
-class TestBuildConceptInfoForStringAttribute(TestBuildConceptInfoForAttribute):
+class TestBuildConceptInfoForStringAttribute(BaseTestBuildConceptInfoForAttribute.TestBuildConceptInfoForAttribute):
 
     query = "match $x isa job-title; get;"
     var = 'x'
