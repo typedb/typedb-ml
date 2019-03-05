f19885de0867555e673d1c226f8384ba7df6e838
James Fletcher <james@grakn.ai>
2019-03-05 21:29:36 +0000

    Correct path to unzipped grakn animaltrade distribution

--- examples/kgcn/animal_trade/test/end_to_end_test.py
+++ examples/kgcn/animal_trade/test/end_to_end_test.py
@@ -82,10 +82,7 @@ class TestEndToEnd(unittest.TestCase):
                           'external/animaltrade_dist/file/downloaded-unzipped'])
 
         # Start Grakn
-        sub.run([
-                    'external/animaltrade_dist/file/downloaded-unzipped/grakn-core-animaltrade'
-                    '-20750ca0a46b4bc252ad81edccdfd8d8b7c46caa/grakn',
-                    'server', 'start'])
+        sub.run(['external/animaltrade_dist/file/downloaded-unzipped/grakn-core-animaltrade-1.5.0/grakn', 'server', 'start'])
 
         modes = (TRAIN, EVAL)
 
