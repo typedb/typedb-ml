c5ce97dc48f32c08f5c4980eb76eb2d123a8a8cf
James Fletcher <james@grakn.ai>
2019-06-04 15:09:57 +0100

    Restrict the nodes and edges plotted to only those in the solution

--- experiment/plotting.py
+++ experiment/plotting.py
@@ -22,15 +22,29 @@ import networkx as nx
 
 
 def plot_with_matplotlib(G):
-    labels_dict = {node_id: G.nodes[node_id]['type'] for node_id in G.nodes}
-    edge_labels_dict = {(edge_id[0], edge_id[1]): G.edges[edge_id]['type'] for edge_id in G.edges}
-    # pos = nx.spring_layout(G, k=6 / math.sqrt(len(G.nodes)))
+
+    attribute = 'solution'
+    edges = []
+    for edge in G.edges:
+        if G.edges[edge][attribute] == 1:
+            edges.append(edge)
+
+    nodes = []
+    for node in G.nodes:
+        if G.nodes[node][attribute] == 1:
+            nodes.append(node)
+
+    labels_dict = {node_id: G.nodes[node_id]['type'] for node_id in nodes}
+    edge_labels_dict = {(edge_id[0], edge_id[1]): G.edges[edge_id]['type'] for edge_id in edges}
+    # pos = nx.spring_layout(G, k=6 / math.sqrt(G.number_of_nodes()))
     pos = nx.circular_layout(G)
-    nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'),
+    nx.draw_networkx_nodes(G, pos,
+                           nodelist=nodes,
+                           cmap=plt.get_cmap('jet'),
                            # node_color=values,
                            node_size=500
                            )
     nx.draw_networkx_labels(G, pos, labels=labels_dict)
-    nx.draw_networkx_edges(G, pos, edgelist=None, edge_color='r', arrows=True, arrowsize=30)
+    nx.draw_networkx_edges(G, pos, edgelist=edges, edge_color='r', arrows=True, arrowsize=30)
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels_dict)
     plt.show()
