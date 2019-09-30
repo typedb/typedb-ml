a03a7c893c0d3a2d3e02aac65b900cfd38edd8e1
James Fletcher <james@grakn.ai>
2019-09-30 13:22:04 +0100

    Alter loss function to only penalise for incorrect nodes, not edges

--- kglib/kgcn/learn/loss.py
+++ kglib/kgcn/learn/loss.py
@@ -32,8 +32,7 @@ def loss_ops_from_difference(target_op, output_ops):
 
     """
     loss_ops = [
-        tf.losses.softmax_cross_entropy(target_op.nodes, output_op.nodes) +
-        tf.losses.softmax_cross_entropy(target_op.edges, output_op.edges)
+        tf.losses.softmax_cross_entropy(target_op.nodes, output_op.nodes)
         for output_op in output_ops
     ]
     return loss_ops
