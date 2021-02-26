51af4f0b2e75e2f65fd5fee12f78ed9ae0c3bf1c
Joren Retel <joren.retel@bayer.com>
2021-02-26 10:58:45 +0100

    remove init file in dir too high up

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='kglib2.0',
     version='',
-    packages=['kglib'],
+    packages=['kglib', 'kglib.utils', 'kglib.kgcn', 'kglib.kgcn_data_loader'],
     url='',
     license='',
     author='henning.kuich@bayer.com',
